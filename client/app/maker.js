ee4de4945828bd210f4287ba795d3e013bf24893
CalzareN <allie.sangalli@gmail.com>
2019-04-14 22:28:36 -0400

    more tests

--- client/app/maker.js
+++ client/app/maker.js
@@ -96,46 +96,22 @@ const TypeList = function(props) {
 	  </div>
 	</div>
   );
-  /*
-  if(props.domos.length === 0){
-		return(
-			<div className="domoList">
-				<h3 className="emptyDomo">No Domos yet</h3>
-			</div>
-		);
-	}
-	
-	const domoNodes = props.domos.map(function(domo){
-		return(
-			<div key={domo._id} className="domo">
-				<img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace"/>
-				<h3 className="domoName"> Name: {domo.name} </h3>
-				<h3 className="domoAge"> Age: {domo.age}</h3>
-			</div>
-		);
-	});
-	return(
-		<div className = "domoList">
-			{domoNodes}
-		</div>
-	);
-	*/
 };
-/*
+
 const loadDomosFromServer = () =>{
 	sendAjax('GET', '/getDomos',null,(data)=>{
 		ReactDOM.render(
-			<DomoList domos={data.domos}/>,document.querySelector("#domos")
+			<TypeList domos={data.domos}/>,document.querySelector(".money")
 		);
 	});
-};*/
+};
 
 const setup = function(csrf) {
 	ReactDOM.render(
 		<TypeList domos={[]}/>, document.querySelector("#domos")
 	);
 	
-	//loadDomosFromServer();
+	loadDomosFromServer();
 };
 
 const getToken = () => {
